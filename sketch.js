55a7e9b57fd6a706e94c6957fe6ade71319190f7
Andrea Andrade <aandrade@wlapcs.org>
2017-04-11 17:43:01 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(600,400);
+}
+
+function draw(){
+    background(95 , 78, 190);
+    //display ball
+    fill(50, 250,50);
+    ellipse(x, 100, 60,60);
+    //make ball move
+    x + x + speedx;
+    //ball changes direction when it hits wall
+    
+}
